94c20e05042645c92dd63b79f476dd0835f5dda1
Paul Sims <paul.q.sims@gmail.com>
2020-07-13 20:49:55 -0400

    Renamed files and created code for transforming data

---
+++ R/data_mod.R
@@ -0,0 +1,31 @@
+################################################################################
+# Function: Modify data for analyses
+# Author: Paul Q. Sims
+# Contact: paul.q.sims@gmail.com
+# Date: 2020
+# Purpose: Population comparison analyses for Sims and Reader 2020
+################################################################################
+
+# Housekeeping 
+
+library(tidyverse)
+
+data_analysis <- 
+  read_csv("data/data_Sims-Reader_2020.csv") %>%
+  mutate(across(where(is.character), ~as_factor(.x))) # Convert characters to factors
+
+# Load custom functions 
+source("r/XX_my-functions.R")
+
+data_analysis_mod <-
+  data_analysis %>%
+  mutate(across(.cols = c(goal_z_lat, body_length,  # log transformations
+                          learn_prop, tot_z),  
+                ~ log(.x),
+                .names = "{col}_LN"),
+         across(.cols = c(body_length, tot_z),  # mean center and scale variables by 1 SD
+                ~ na_rm_scale(.x, na.rm = T),
+                .names = "{col}_sc"))
+
+write_csv(data_analysis_mod,
+          path = "data/data_Sims-Reader_2020_mod.csv")
