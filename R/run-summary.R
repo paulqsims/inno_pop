05756238d33fd595928abd2b8e95a83fb4614c69
Paul Sims <paul.q.sims@gmail.com>
2020-07-22 17:06:42 -0400

    Create render file for Rmd files to knit to location

--- R/run-summary.R
+++ R/run-summary.R
@@ -1,1 +1,7 @@
-getwd()
+# Markdown and HTML generation 
+rmarkdown::render("R/analysis_pop-comp.Rmd", 
+                  output_dir = "reports/")
+rmarkdown::render("R/analysis_inno-predict.Rmd", 
+                  output_dir = "reports/")
+rmarkdown::render("R/analysis-summary.Rmd", 
+                  output_dir = "reports/")
