c50dd1f56e2f1cfc4c090adc923d1e0154e8d6b8
Paul Sims <paul.q.sims@gmail.com>
2020-08-01 22:44:25 -0400

    Simplify and annotate functions, Broke large function tidy predictor table into smaller functions

--- R/custom-functions.R
+++ R/custom-functions.R
@@ -5,20 +5,21 @@
 # Date: 2020
 ###############################################################################
 
+
+#### Data manipulation ####
+
 # Mean centers and standardizes (1 SD) vector with NA value removal option
-#
-# Note that the default is False so removal must be specified
-# scale() from base R does not allow this
+#  - Note that the default is False so removal must be specified
+#  - scale() from base R does not allow this
 
 na_rm_scale <- function(x, na.rm = FALSE) {
   (x - mean(x, na.rm = na.rm)) / sd(x, na.rm)
 }
 
 # Reads in data and converts characters to factors
-# Also mutates variables
-#
-# Only need to input file name without extension in quotes
-# Assumes file is in the data directory and files are .csv 
+#  - Also mutates variables
+#  - Only need to input file name without extension in quotes
+#  - Assumes file is in the data directory and files are .csv 
 
 read_mod_data <- function(data) {
   read_csv(paste0("data/", data, ".csv", sep = "")) %>%
@@ -33,12 +34,12 @@ read_mod_data <- function(data) {
                   .names = "{col}_sc"))
 }
 
-# Rounding functions ----
+#### Rounding functions ####
 
 # Rounds a vector of p-values
-#
-# Distinguishes between 0 and <0.001
-# Output is character vector!
+#  - Rounds based on p-value size
+#  - Distinguishes between 0 and <0.001
+#  - Output is character vector!
 
 round_pval <- function(p_value) {
   p_value <- ifelse(p_value < 0.001, "<0.001",
@@ -48,10 +49,9 @@ round_pval <- function(p_value) {
   p_value <- as.character(p_value)  # change to character to make compatible w/ df that have P <0.001
   }
 
-# Rounds a continuous vector statistic
-# 
-# Distinguishes between 0 and < 0.01
-# Output is character vector!
+# Rounds a continuous vector statistic to 2 decimal places
+#  - Distinguishes between 0 and < 0.01 
+#  - Output is character vector!
 
 round_est <- function(estimate) {
   estimate <- ifelse(estimate < 0.01 & estimate > 0, "<0.01",
@@ -61,7 +61,20 @@ round_est <- function(estimate) {
   estimate <- as.character(estimate) # change to character to make compatible w/ df that have estimates < 0.01
 }
 
+# Round tidy model output  
+#  - Rounds estimates to 2, p-values to 3
+rd_tidy_out <- function(tidyOutput) {
+  tidyOutput %>%
+    mutate(across(.cols = c(estimate:statistic), ~ round_est(.x)), 
+           p.value = round_pval(p.value)) %>%
+    return(.)
+}
+
+#### Kable and Tidy functions ####
+
 # Print pretty kable tables
+#  - Colors every other row
+#  - Aligns cell text to the left within the table
 pretty_kable <- function(kableOutput) {
   kableOutput %>%
     kableExtra::kable_styling(bootstrap_options = c("striped", "hover",
@@ -69,35 +82,73 @@ pretty_kable <- function(kableOutput) {
                               position = "left")
 }
 
+# Kable title and alignment
+#  - Creates title and aligns table location on page
+kable_title <- function(kableTable, title) {
+  knitr::kable(kableTable, align = "l",
+               caption = if (!is.null(title)) {
+                 paste(title)
+               } else { paste("") }) %>%
+  return(.)
+}
+
 # Tidy predictor tables
+#  - Tidies model output depending on mixed or non-mixed
+#  - Creates title and output can be df or kable for pretty kable
 pretty_PredictTab <- function(modelOutput, title = NULL,
                               mixedModel = FALSE, kable = TRUE) {
-  # kable = whether or not a kable should be printed
+  # Arguments
+  #  kable = whether or not a kable should be printed
   if (mixedModel == TRUE) {
-      broom.mixed::tidy(modelOutput,
-                        effects = "fixed") %>%
-      mutate(across(.cols = c(estimate:statistic), ~round_est(.x)), 
-             p.value = round_pval(p.value)) %>%
-      {if (kable == TRUE) {
-        knitr::kable(., align = "l",
-                     caption = if (!is.null(title)) {
-                       paste(title)
-                     } else { paste("") }
-        ) %>%
-          pretty_kable(.)
-      } else { print(.) }}
-  } else {
+    broom.mixed::tidy(modelOutput,
+                      effects = "fixed") %>%
+    rd_tidy_out(.) %>%  # round tidy output
+      {if (kable == TRUE) {  # return kable
+        kable_title(., title) %>%  # modify kable title and alignment
+          pretty_kable(.)  # pretty kable
+      } else { return(.) }}  # return dataframe, not kable
+  } else {  # if not a mixed model
     broom::tidy(modelOutput) %>%
-    mutate(across(.cols = c(estimate:statistic), ~round_est(.x)), 
-           p.value = round_pval(p.value)) %>%
-      {if (kable == TRUE) {
-        knitr::kable(., align = "l",
-                     caption = if (!is.null(title)) {
-                       paste(title)
-                     } else { paste("") }
-        ) %>%
-          pretty_kable(.)
-      } else { print(.) }}
+      rd_tidy_out(.) %>%  # round tidy output
+        {if (kable == TRUE) {
+          kable_title(., title) %>%  # modify kable title and alignment
+            pretty_kable(.)  # pretty kable
+        } else { return(.) }}  # return dataframe, not kable
   }
 }
+
+
+
+
+# Tidy predictor tables
+# pretty_PredictTab <- function(modelOutput, title = NULL,
+#                               mixedModel = FALSE, kable = TRUE) {
+#   # kable = whether or not a kable should be printed
+#   if (mixedModel == TRUE) {
+#       broom.mixed::tidy(modelOutput,
+#                         effects = "fixed") %>%
+#       mutate(across(.cols = c(estimate:statistic), ~round_est(.x)), 
+#              p.value = round_pval(p.value)) %>%
+#       {if (kable == TRUE) {
+#         knitr::kable(., align = "l",
+#                      caption = if (!is.null(title)) {
+#                        paste(title)
+#                      } else { paste("") }
+#         ) %>%
+#           pretty_kable(.)
+#       } else { return(.) }}
+#   } else {
+#     broom::tidy(modelOutput) %>%
+#     mutate(across(.cols = c(estimate:statistic), ~round_est(.x)), 
+#            p.value = round_pval(p.value)) %>%
+#       {if (kable == TRUE) {
+#         knitr::kable(., align = "l",
+#                      caption = if (!is.null(title)) {
+#                        paste(title)
+#                      } else { paste("") }
+#         ) %>%
+#           pretty_kable(.)
+#       } else { return(.) }}
+#   }
+# }
 
